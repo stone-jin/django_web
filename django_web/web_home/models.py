f397735afd711e14acd0e659f324022d80164a05
金炳 <hzjinbing@corp.netease.com>
2016-05-15 11:44:54 +0800

    misc: 增加到后台可视化中，并且增加了测试的账号stone-jin,密码:test123456

--- django_web/web_home/models.py
+++ django_web/web_home/models.py
@@ -1,5 +1,17 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class OpenID(models.Model):
+    user_email = models.EmailField(unique=True)
+    time = models.TimeField()
+
+    def __unicode__(self):
+        return self.user_email
+
+    class Meta:
+        verbose_name = u'用户的邮箱'
+        verbose_name_plural = u'用户的邮箱'
+
